819ba7e17bab631c24477458799d9d2f07d51dad
Tim Haak <tim@haak.co>
2021-06-15 15:43:46 +0200

    Cleanup and fix problems

--- infra/terraform/apply.sh
+++ infra/terraform/apply.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
+
+export DNS_DOMAIN='purplesocks.net'
 export TRAEFIK_USERNAME='traefik'
 export TRAEFIK_PASSWD='yairohchahKoo0haem0d'
 export GRAFANA_ADMIN_PASSWORD='example_password'
-
-export DNS_DOMAIN='purplesocks.net'
 
 if [[ -z ${CLOUDFLARE_API_TOKEN} ]]; then
   echo "Please enter the CLOUDFLARE_API_TOKEN or set the env variable: "
@@ -11,6 +11,19 @@ if [[ -z ${CLOUDFLARE_API_TOKEN} ]]; then
 else
   echo "Read CLOUDFLARE_API_TOKEN from env"
 fi
+export CLOUDFLARE_API_TOKEN
+
+export TF_VAR_cloudflare_api_token="${CLOUDFLARE_API_TOKEN}"
+
+if [[ -z ${DIGITALOCEAN_TOKEN} ]]; then
+  echo "Please enter the DIGITALOCEAN_TOKEN or set the env variable: "
+  read -r DIGITALOCEAN_TOKEN
+else
+  echo "Read DIGITALOCEAN_TOKEN from env"
+fi
+export DIGITALOCEAN_TOKEN
+
+export TF_VAR_do_token="${DIGITALOCEAN_TOKEN}"
 
 export TF_VAR_dns_domain="${DNS_DOMAIN}"
 
--- infra/terraform/destroy.sh
+++ infra/terraform/destroy.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
-if [[ -z ${CLOUDFLARE_API_TOKEN} ]] ; then
+if [[ -z ${CLOUDFLARE_API_TOKEN} ]]; then
   echo "Please enter the CLOUDFLARE_API_TOKEN or set the env variable: "
   read -r CLOUDFLARE_API_TOKEN
 else
   echo "Read CLOUDFLARE_API_TOKEN from env"
 fi
+export CLOUDFLARE_API_TOKEN
+
+export TF_VAR_cloudflare_api_token="${CLOUDFLARE_API_TOKEN}"
+
+if [[ -z ${DIGITALOCEAN_TOKEN} ]]; then
+  echo "Please enter the DIGITALOCEAN_TOKEN or set the env variable: "
+  read -r DIGITALOCEAN_TOKEN
+else
+  echo "Read DIGITALOCEAN_TOKEN from env"
+fi
+export DIGITALOCEAN_TOKEN
+
+export TF_VAR_do_token="${DIGITALOCEAN_TOKEN}"
 
 export TF_VAR_dns_domain=""
 export TF_VAR_traefik_username=""
